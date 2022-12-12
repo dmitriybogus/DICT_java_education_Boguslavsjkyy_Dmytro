0c159090d82fffad0061da26c01588e3b28aca7e
dmytro.bogyslavsjkyy <d.a.boguslavsjkyy@student.khai.edu>
2022-12-12 20:37:25 +0300

    no commands remaining

--- src/Test.java
+++ src/Test.java
@@ -1,5 +1,12 @@
 public class Test {
+<<<<<<< HEAD
     public static void main(String[] args) {
         System.out.println("Java education");
+=======
+    public static void main (String [] args) {
+        System.out.println("education");
+        //new string
+        System.out.println("DICT");
+>>>>>>> 7d93309 (Test.java added to feature_branch with change on string #3)
     }
 }
