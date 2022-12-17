a38e56cdec6862001314a9b71f4b9496607b896a
dmytro.bogyslavsjkyy <d.a.boguslavsjkyy@student.khai.edu>
2022-12-17 16:26:38 +0300

    ChatBot 1-st stage

---
+++ src/Chatbot/ChatBot.java
@@ -0,0 +1,8 @@
+package Chatbot;
+
+public class ChatBot {
+    public static void main(String [] args){
+        System.out.println("Hello! My name is " + "Jarvis" + ".");
+        System.out.println("I was created in " + "2022" + ".");
+    }
+}
